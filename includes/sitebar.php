d7be8e683d885aedae96911c092b9c6d09b261c0
RobertPurc <robert.purc12@gmail.com>
2020-09-17 09:11:55 +0200

    serach engine done

--- includes/sitebar.php
+++ includes/sitebar.php
@@ -1,28 +1,4 @@
 <div class="col-md-4">
-<?php 
-
-if(isset($_POST['submit'])){
-$search = $_POST['search']; 
-
-$query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
-
-$search_query = mysqli_query($conn, $query);
-
-if(!$search_query){
-    die('Quuery failed' . mysqli_error($conn));
-}
-
-$count = mysqli_num_rows($search_query);
-
-if($count == 0){
-    echo'<h1> no result</h1>';
-} else {
-    echo 'some result';
-}
-
-}
-
- ?>
 
 
 
@@ -32,7 +8,7 @@ if($count == 0){
 <!-- Blog Search Well -->
 <div class="well">
     <h4>Blog Search</h4>
-    <form action="" method="post">
+    <form action="search.php" method="post">
     <div class="input-group">
         <input name="search" type="text" class="form-control">
         <span class="input-group-btn">
@@ -46,42 +22,43 @@ if($count == 0){
     <!-- /.input-group -->
 </div>
 
+
+
+
+
+
 <!-- Blog Categories Well -->
 <div class="well">
     <h4>Blog Categories</h4>
     <div class="row">
         <div class="col-lg-6">
             <ul class="list-unstyled">
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
+
+
+            <?php
+
+$query = "SELECT *  FROM categories LIMIT 3";
+$select_sitebar_categories_query = mysqli_query($conn, $query);
+
+while($row = mysqli_fetch_assoc($select_sitebar_categories_query)) {
+        $cat_title = $row['cat_title'];
+
+        echo "<li><a href='{$cat_title}'>{$cat_title} </a></li>";
+}
+
+?>
             </ul>
         </div>
         <!-- /.col-lg-6 -->
-        <div class="col-lg-6">
-            <ul class="list-unstyled">
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /.col-lg-6 -->
+        
     </div>
     <!-- /.row -->
 </div>
 
 <!-- Side Widget Well -->
 <div class="well">
+
+<?php include "widget.php" ?>
     <h4>Side Widget Well</h4>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
 </div>

6c4740a832fe9b8127702fd9a13cda2a368f46c4
RobertPurc <robert.purc12@gmail.com>
2020-10-26 19:22:44 +0100

    work from mobile

---
+++ admin/includes/view_all_comments.php
@@ -0,0 +1,91 @@
+<table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Author</th>
+                                    <th>Comment</th>
+                                    <th>Email</th>
+                                    <th>Status</th>
+                                    <th>In Response</th>
+                                    <th>Date</th>
+                                    <th>Approve</th>
+                                    <th>Unapproved</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $query = "SELECT * FROM comments";
+
+                                $select_posts = mysqli_query($conn, $query);
+
+                                while (
+                                  $row = mysqli_fetch_assoc($select_posts)
+                                ) {
+                                  $comment_id = $row['comment_id'];
+                                  $comment_author = $row['comment_author'];
+                                  $comment_content = $row['comment_content'];
+                                  $comment_status = $row['comment_status'];
+                                  $comment_post_id = $row['comment_post_id'];
+                                  $comment_email = $row['comment_email'];  
+                                  $comment_date = $row['comment_date'];
+                                
+
+                                  echo "<tr>";
+                                  echo "<td>{$comment_id}</td>";
+                                  echo "<td>{$comment_author}</td>";
+                                  echo "<td>{$comment_content}</td>";
+
+                                  
+//  $query = "SELECT * FROM  comments WHERE comment_id = {$comment_post_id}";
+//                                     $select_categories_id = mysqli_query(
+//                                       $conn,
+//                                       $query
+//                                     );
+//                                     while (
+//                                       $row = mysqli_fetch_assoc(
+//                                         $select_categories_id
+//                                       )
+//                                     ) {
+   
+//                                       $cat_id = $row['cat_id'];
+//                                       $cat_title = $row['cat_title'];
+                                     
+//                                   echo "<td>{$cat_title}</td>";
+//                                     }
+                                  
+
+
+echo "<td>{$comment_email}</td>";
+echo "<td>{$comment_status}</td>";
+echo "<td>Some title</td>";                                 
+echo "<td>{$comment_date}</td>";
+
+echo "<td><a href='posts.php?source=edit_post&p_id='>Approve</a></td>";
+echo "<td><a href='posts.php?source=edit_post&p_id='>Unapprove</a></td>";
+echo "<td><a href='posts.php?source=edit_post&p_id='>Delete</a></td>";
+
+                                 
+                                
+                                
+                                 echo "</tr>";
+                                }
+                                ?>
+                                  
+                                
+                            </tbody>
+                        </table>
+
+                        <?php
+if(isset($_GET['delete'])){
+$the_post_id = $_GET['delete'];
+
+$query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
+
+$delete_query = mysqli_query($conn, $query);
+
+header("Location: posts.php");
+
+}
+
+?>
